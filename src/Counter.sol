494e5c770c3cececf27fedbb176fdd3e5e665271
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-07-25 10:40:21 +0530

    Add deployer as the owner

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address public owner;
+
+    //deployer is set as the owner
+    constructor() {
+        owner = msg.sender;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
